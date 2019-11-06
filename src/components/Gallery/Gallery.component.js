5909616eefa096e546a95ae489ca0d330bf8f1a8
Ben Terry <benterry@Bens-MacBook.local>
2019-11-06 14:54:09 -0500

    built function into gallery

--- src/components/Gallery/Gallery.component.js
+++ src/components/Gallery/Gallery.component.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // import styles
 import styles from './Gallery.module.css'
 
 const Gallery = ({images}) => {
+    const [featured, setFeatured] = useState(images[0])
+    const handleNewFeatured = (e) => {
+        setFeatured(images[e.target.getAttribute('index')])
+    }
     return (
         <div>
             <div className={styles.selectedImage}>
-                <img src={images[0].src} alt={images[0].caption} />
+                <img src={featured.src} alt={featured.caption} />
                 <p>{images[0].caption}</p>
             </div>
             <div className={styles.gallery}>
-                {images.map((image) => 
+                {images.map((image, index) => 
                     <div
                         key={`${image.src}`}
                         style={{
@@ -20,7 +24,9 @@ const Gallery = ({images}) => {
                             backgroundPosition: 'center',
                             backgroundRepeat: 'no-repeat'
                         }}
-                        className={styles.unselected}
+                        className={image.src === featured.src ? undefined : styles.unselected}
+                        onClick={(e) => handleNewFeatured(e)}
+                        index={index}
                     />
                 )}
             </div>
