10ee2d0e8e5009040083c665f625da0e1878a796
Ben Terry <beterry12@gmail.com>
2019-11-18 20:13:16 -0500

    Updates

---
+++ precache-manifest.d146240c6661109e69c0bd48a15bd3c7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0780f496e255acc3ae4eabefe3a3b9",
+    "url": "/christmas-card-2019/index.html"
+  },
+  {
+    "revision": "8507e96ee4abc8b50378",
+    "url": "/christmas-card-2019/static/css/main.dc3604b0.chunk.css"
+  },
+  {
+    "revision": "d4d43c2474ea0ffec9ba",
+    "url": "/christmas-card-2019/static/js/2.f8a6f179.chunk.js"
+  },
+  {
+    "revision": "8507e96ee4abc8b50378",
+    "url": "/christmas-card-2019/static/js/main.daeeb9cf.chunk.js"
+  },
+  {
+    "revision": "65e626aa71689a6ed9db",
+    "url": "/christmas-card-2019/static/js/runtime-main.d5fcc075.js"
+  },
+  {
+    "revision": "976b4aa6d271ea0d35056371b4afb81d",
+    "url": "/christmas-card-2019/static/media/background.976b4aa6.svg"
+  },
+  {
+    "revision": "71abb4a3af9e3a4f05fb12761ac558c0",
+    "url": "/christmas-card-2019/static/media/image-ap-1.71abb4a3.jpg"
+  },
+  {
+    "revision": "0531101b3f31b434ea4136b2fb749a58",
+    "url": "/christmas-card-2019/static/media/image-ap-2.0531101b.jpg"
+  },
+  {
+    "revision": "5eb1828f0d7275ba023212b38a09ac97",
+    "url": "/christmas-card-2019/static/media/image-ap-3.5eb1828f.jpg"
+  },
+  {
+    "revision": "b2ff4dafba0381fdb5708ac77a5ab140",
+    "url": "/christmas-card-2019/static/media/image-ap-4.b2ff4daf.jpg"
+  },
+  {
+    "revision": "2caf7b144e4c5c0a11c084a8c9a73080",
+    "url": "/christmas-card-2019/static/media/image-ap-5.2caf7b14.jpg"
+  },
+  {
+    "revision": "23f747a44658e49648619553dca5a329",
+    "url": "/christmas-card-2019/static/media/image-ap-6.23f747a4.jpg"
+  },
+  {
+    "revision": "8dd83c2482af7708dde5b7650fb99dea",
+    "url": "/christmas-card-2019/static/media/image-ben-1.8dd83c24.jpg"
+  },
+  {
+    "revision": "a69e0e2d442d6db6de1dc62ddfbf0a45",
+    "url": "/christmas-card-2019/static/media/image-ben-2.a69e0e2d.jpg"
+  },
+  {
+    "revision": "703dd92e7d8bd2de36abdb523fd82464",
+    "url": "/christmas-card-2019/static/media/image-ben-3.703dd92e.jpg"
+  },
+  {
+    "revision": "5e4992cc0c02107e720068100fa62584",
+    "url": "/christmas-card-2019/static/media/image-ben-4.5e4992cc.jpg"
+  },
+  {
+    "revision": "3449b5c475c4ffbc86c21696890ee5c4",
+    "url": "/christmas-card-2019/static/media/image-ben-5.3449b5c4.jpg"
+  },
+  {
+    "revision": "98f131a7e242010e7e5f2c10ad32e435",
+    "url": "/christmas-card-2019/static/media/image-ben-6.98f131a7.png"
+  },
+  {
+    "revision": "45263ec9ae0ea7640cbca4eca9e9e3b9",
+    "url": "/christmas-card-2019/static/media/image-jake-1.45263ec9.jpg"
+  },
+  {
+    "revision": "e50bf4790618348813793eced8aa2e0a",
+    "url": "/christmas-card-2019/static/media/image-jake-2.e50bf479.jpg"
+  },
+  {
+    "revision": "dce6a323fd1506dd85df21e885424b69",
+    "url": "/christmas-card-2019/static/media/image-jake-3.dce6a323.jpg"
+  },
+  {
+    "revision": "1472bc8212d498892258e988208160c9",
+    "url": "/christmas-card-2019/static/media/image-jake-4.1472bc82.jpg"
+  },
+  {
+    "revision": "4a9ae5517e504ae328972053c235e3db",
+    "url": "/christmas-card-2019/static/media/image-jake-5.4a9ae551.jpg"
+  },
+  {
+    "revision": "d7c206d1dead4194d1ba5fb0a9d3acbd",
+    "url": "/christmas-card-2019/static/media/image-jake-6.d7c206d1.jpg"
+  }
+]);
